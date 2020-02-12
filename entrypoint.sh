92206e63fbba03099a3918f28b05f315b212ecea
James Hegedus <james@jthegedus.com>
2020-02-12 13:18:56 +1100

    fix: logging of cmd execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 echo "[INFO] Installing awesome-lint@${AWESOME_LINT_VERSION}"
 npm install -g awesome-lint@${AWESOME_LINT_VERSION}
 
+echo "[INFO] Executing:"
 echo "[INFO] awesome-lint $*"
 sh -c "awesome-lint $*"
